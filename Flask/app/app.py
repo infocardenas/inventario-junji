c93edf6c2cf7a7a0e06549004e6305f246e121b3
EstebanBra <esteban.bravo2101@alumnos.ubiobio.cl>
2024-12-12 10:27:17 -0300

    Cambios

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -3,5 +3,9 @@ from flask import Flask
 # crea una instancia de la aplicación Flask, utilizando el nombre del módulo actual para configurar la aplicación correctamente.
 app = Flask(__name__)
 
+# Habilitar debug mode
+if __name__ == "__main__":
+    app.run(debug=True)
+    
 # protege las cookies y datos de sesion del usuario
 app.secret_key = "mysecretkey"
