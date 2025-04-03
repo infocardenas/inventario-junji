09896887622c0296002ae40f664abab54453845b
Isaac Bravo <is.bravo@duocuc.cl>
2025-04-03 16:15:19 -0300

    version 0.0.2

---
+++ addData.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO `orden_compra` (`idOrden_compra`, `nombreOrden_compra`, `fechacompraOrden_compra`, `fechafin_ORDEN_COMPRA`, `rutadocumentoOrden_compra`, `idTipo_adquisicion`, `idProveedor`) VALUES
+('OC001', 'Compra Equipos', '2024-04-01', '2024-04-15', '/ruta', 1, 2),
+('OC002', 'Compra Muebles', '2024-03-20', '2024-04-10', '/ruta', 2, 3),
+('OC003', 'Compra Software', '2024-03-25', '2024-04-20', '/ruta', 3, 1);
