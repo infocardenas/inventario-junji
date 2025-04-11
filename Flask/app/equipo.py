2674bbd88eba985c6972aa8d7288b50e00d0dd6a
Isaac Bravo <is.bravo@duocuc.cl>
2025-04-11 11:10:48 -0400

    recuperar

--- Flask/app/equipo.py
+++ Flask/app/equipo.py
@@ -13,7 +13,7 @@ import os
 equipo = Blueprint("equipo", __name__, template_folder="app/templates")
 
 
-# envia datos al formulario y tabla de equipo CAMBIA FK_IDCODIGO_PROVEEDOR 
+# envia datos al formulario y tabla de equipo CAMBIA FK_IDCODIGO_PROVEEDOR sd
 @equipo.route("/equipo")
 @loguear_requerido
 def Equipo():
