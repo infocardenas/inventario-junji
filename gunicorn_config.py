914bcb6fca8eb6aa5cdc40aa8a1d8e127551fc8b
Bastian-dev <b.arriagada2201@alumnos.ubiobio.cl>
2025-05-26 12:11:57 -0400

    Actualizacion configuracion de gunicorn (logging)

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -11,3 +11,15 @@ chdir = os.path.abspath(os.path.join(os.path.dirname(__file__), "Flask"))
 # chdir = "./Flask" # Podrías usar esta también si corres desde ~/inventario-junji/
 
 wsgi_app = "app.wsgi:app"
+
+# --- Configuración de Logging ---
+accesslog = "-"  # stdout
+errorlog = "-"   # stderr
+
+# Opciones para loglevel: 'debug', 'info', 'warning', 'error', 'critical'
+# 'info' mostrará inicios de workers, advertencias SSL, y errores de aplicación.
+# 'error' ocultará las advertencias SSL pero también los mensajes informativos de Gunicorn.
+loglevel = 'error' 
+
+# Formato detallado para el log de acceso
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" ProcessTime:%(L)s PID:%(p)s'
