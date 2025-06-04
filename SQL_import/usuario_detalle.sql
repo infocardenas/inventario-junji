9550a8f60db586390ffe01212022711f96650793
Isaac Bravo <is.bravo@duocuc.cl>
2025-06-04 15:18:11 -0400

    propuesta de detalle de usuaurio  2

--- SQL_import/usuario_detalle.sql
+++ SQL_import/usuario_detalle.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `detalle_usuario` (
   `nombreUsuario` varchar(250) NOT NULL,
   `rut` varchar(20) DEFAULT NULL,
-  `imagen` varchar(250) DEFAULT NULL,
   `cargo` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`nombreUsuario`),
   CONSTRAINT `fk_detalle_usuario_usuario` FOREIGN KEY (`nombreUsuario`) REFERENCES `usuario` (`nombreUsuario`) ON DELETE CASCADE
