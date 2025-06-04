275292a755f9adbb3241d91e17d6e7d9edeb1143
Isaac Bravo <is.bravo@duocuc.cl>
2025-06-04 13:11:06 -0400

    propuesta de detalle de usuaurio

---
+++ SQL_import/usuario_detalle.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `detalle_usuario` (
+  `nombreUsuario` varchar(250) NOT NULL,
+  `rut` varchar(20) DEFAULT NULL,
+  `imagen` varchar(250) DEFAULT NULL,
+  `cargo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`nombreUsuario`),
+  CONSTRAINT `fk_detalle_usuario_usuario` FOREIGN KEY (`nombreUsuario`) REFERENCES `usuario` (`nombreUsuario`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
